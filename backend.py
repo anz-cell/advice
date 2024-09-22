8b2be401fe432b0705ec0bafda6283352aa30aad
anz-cell <153947658+anz-cell@users.noreply.github.com>
2024-09-22 12:32:02 +0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -252,7 +252,7 @@ def create_report_english(data, recommendations):
             for key, value in Recommendation_English.items():
                 if key in data:
                     if data[f'dropdown_{key}'] == priority:
-                        string = fr'{value[0]} in {data[f'input_{key}']}'
+                        string = fr'{value[0]} in {data[f"input_{key}"]}'
                         row_cells = recommendations_table.add_row().cells
                         row_cells[0].text = (string)
                         row_cells[1].text = (value[1])
@@ -472,7 +472,7 @@ def create_report_arabic( data, recommendations):
             for key, value in Recommendation_Arabic.items():
                 if key in data:
                     if data[f'dropdown_{key}'] == priority:
-                        string = f'{value[0]} في {data[f'input_{key}']}'
+                        string = f'{value[0]} في {data[f"input_{key}"]}'
                         row_cells = recommendations_table.add_row().cells
                         row_cells[0].text = (string)
                         row_cells[1].text = (value[1])
